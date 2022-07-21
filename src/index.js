d771914a97a01e72eedbf49dda6a0b5c48152505
Taras Slovak <slovak.taras@gmail.com>
2022-07-21 12:15:20 +0300

    added state and refactored

--- src/index.js
+++ src/index.js
@@ -1,61 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-} from '@apollo/client';
+
+import { ApolloProvider } from '@apollo/client';
+import { Provider } from 'react-redux';
+import { client } from './query';
+import { store } from './state/store';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
-  cache: new InMemoryCache(),
-});
-
-client
-  .query({
-    query: gql`
-    query GetCategories {
-      categories {
-        name
-        products{
-          id
-          name
-          inStock
-          gallery
-          description
-          category
-          attributes{
-            id
-            name
-            type
-            items{
-              displayValue
-              value
-              id
-            }
-          }
-          prices{
-            currency{
-              label
-              symbol
-            }
-          }
-          brand
-        }
-      }
-    }
-    `,
-  })
-  .then((result) => console.log(result));
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <Provider store={store}>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Provider>,
 );
 
 reportWebVitals();
